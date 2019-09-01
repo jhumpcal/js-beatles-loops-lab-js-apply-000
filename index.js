3975b442974f249085c24d591a46c73098e7b88d
Julian <julianmcalhoun@gmail.com>
2019-09-01 19:42:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(){
 
 function johnLennonFacts(jLFacts){
   const jLFacts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var jLFacts = [];
+  var facts = [];
   var i = 0; 
   while (i < jLFacts.length)
     {
