b5c640bc81cb0bde4e3cfc7ae2a16f2e60cb1cfb
Julian <julianmcalhoun@gmail.com>
2019-09-01 19:35:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+function theBeatlesPlay(){
+  var array = ["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"];
+  
+  return array 
+}
+
+function johnLennonFacts(){
+  var array = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var jLFacts = [];
+  var i = 0; 
+  while (i < jLFacts.length)
+    {
+      johnLennonFacts.push(array[count] + "!!!");
+      i++;
+    }
+  return array 
+}
+
+function iLoveTheBeatles(number){
+  var love = []
+  var number = 0 
+  do {
+    love.push("I love the Beatles!")
+    number++
+  }
+  while (number < 8) {
+    
+  return love;
+  }
+  
+  if (number > 17){
+    
+  return "I love the Beatles!";
+  }
+  
+}
